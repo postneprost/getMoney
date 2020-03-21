5ee3cf995839db92149788e921c71aa5b0171092
postneprost <postneprost@yandex.ru>
2020-03-21 16:23:06 +0500

    Update getMoney.js
    real finish)

--- getMoney.js
+++ getMoney.js
@@ -5,7 +5,6 @@ function getMoney(sum, countMoney) {
     if (sum % 50 != 0) return 'Введите сумму кратную 50';
 
     let allMoney = 0;
-
     for (key in countMoney) {
         allMoney += key * countMoney[key];
     }
@@ -21,7 +20,6 @@ function getMoney(sum, countMoney) {
         50: 0
     }
 
-
     getCountBanknotes(5000);
     getCountBanknotes(1000);
     getCountBanknotes(500);
@@ -30,32 +28,6 @@ function getMoney(sum, countMoney) {
 
     if (restMoney != 0) return 'Недостаточно средств в банкомате';
 
-    function getCountBanknotes(banknote) {
-        if (isBanknotesExist(countMoney, `${banknote}`)) {
-            if (Math.floor(restMoney / banknote) <= countMoney[banknote]) {
-                money[banknote] = Math.floor(restMoney / banknote);
-                restMoney = restMoney - money[banknote] * banknote;
-            } else
-            {
-                money[banknote] = countMoney[banknote];
-                restMoney = restMoney - countMoney[banknote] * banknote;
-            }
-        } else {
-            restMoney = restMoney;
-        }
-    }
-
-    function isBanknotesExist(countMoney, banknote) {
-        return ((banknote in countMoney) && (countMoney[banknote] != 0));
-    }
-
-    getCountBanknotes(5000);
-    getCountBanknotes(1000);
-    getCountBanknotes(500);
-    getCountBanknotes(100);
-    getCountBanknotes(50);
-
-    if (restMoney != 0) return 'Недостаточно средств в банкомате';
     function getCountBanknotes(banknote) {
         if (isBanknotesExist(countMoney, `${banknote}`)) {
             if (Math.floor(restMoney / banknote) <= countMoney[banknote]) {
@@ -77,3 +49,14 @@ function getMoney(sum, countMoney) {
 
     return money;
 }
+
+
+let testMoney = {
+    5000: 10,
+    1000: 10,
+    500: 10,
+    100: 10,
+    50: 10,
+}
+
+console.log(getMoney(66450, testMoney));
