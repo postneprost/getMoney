62040d4eabce4a66b922162a29777c4b8c2669b9
postneprost <postneprost@yandex.ru>
2020-03-21 16:00:24 +0500

    Update getMoney.js
    finish

--- getMoney.js
+++ getMoney.js
@@ -1,5 +1,17 @@
 function getMoney(sum, countMoney) {
 
+    if (sum == 0) return 'Введите необходимую сумму';
+
+    if (sum % 50 != 0) return 'Введите сумму кратную 50';
+
+    let allMoney = 0;
+    for (key in countMoney) {
+        allMoney += key * countMoney[key];
+    }
+    if (allMoney < sum) return 'Недостаточно средств в банкомате';
+
+    let restMoney = sum;
+
     let money = {
         5000: 0,
         1000: 0,
@@ -8,45 +20,31 @@ function getMoney(sum, countMoney) {
         50: 0
     }
 
-    if (isBanknotesExist(countMoney, '5000')) {
-        if (Math.floor(sum / 5000) <= countMoney[5000]) money[5000] = Math.floor(sum / 5000);
-            else money[5000] = countMoney[5000];
+    getCountBanknotes(5000);
+    getCountBanknotes(1000);
+    getCountBanknotes(500);
+    getCountBanknotes(100);
+    getCountBanknotes(50);
+
+    if (restMoney != 0) return 'Недостаточно средств в банкомате';
+    function getCountBanknotes(banknote) {
+        if (isBanknotesExist(countMoney, `${banknote}`)) {
+            if (Math.floor(restMoney / banknote) <= countMoney[banknote]) {
+                money[banknote] = Math.floor(restMoney / banknote);
+                restMoney = restMoney - money[banknote] * banknote;
+            } else
+            {
+                money[banknote] = countMoney[banknote];
+                restMoney = restMoney - countMoney[banknote] * banknote;
+            }
+        } else {
+            restMoney = restMoney;
+        }
     }
 
-    if (isBanknotesExist(countMoney, '1000')) {
-        if (Math.floor((sum % 5000) / 1000) <= countMoney[1000]) money[1000] = Math.floor((sum % 5000) / 1000);
-            else money[1000] = countMoney[1000];
-    }
-
-    if (isBanknotesExist(countMoney, '500')) {
-        if (Math.floor(((sum % 5000) % 1000) / 500) <= countMoney[500]) money[500] = Math.floor(((sum % 5000) % 1000) / 500);
-            else money[500] = countMoney[500];
-    }
-
-    if (isBanknotesExist(countMoney, '100')) {
-        if (Math.floor((((sum % 5000) % 1000) % 500) / 100) <= countMoney[100]) money[100] = Math.floor((((sum % 5000) % 1000) % 500) / 100);
-            else money[100] = countMoney[100];
-    }
-
-    if (isBanknotesExist(countMoney, '50')) {
-        if (Math.floor(((((sum % 5000) % 1000) % 500) % 100) /50) <= countMoney[500]) money[50] = Math.floor(((((sum % 5000) % 1000) % 500) % 100) /50);
-            else money[50] = countMoney[50];
+    function isBanknotesExist(countMoney, banknote) {
+        return ((banknote in countMoney) && (countMoney[banknote] != 0));
     }
 
     return money;
 }
-
-function isBanknotesExist(countMoney, banknote) {
-    return ((banknote in countMoney) && (countMoney[banknote] != 0));
-}
-
-let countBanknotes = {
-    5000: 1,
-    1000: 9,
-    500: 9,
-    100: 9,
-    50: 9
-}
-
-
-console.log(getMoney(18999, countBanknotes));
